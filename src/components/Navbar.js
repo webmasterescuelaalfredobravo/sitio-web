00ae35f2803f3b677cef5f4afb82cdeb0383876d
Gustavo Mateos (Lenovo Laptop) <mateos.gustavo@gmail.com>
2020-09-29 16:21:37 -0300

    grafismos en menú mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import imgbg01 from '../img/elem-menu-01.svg';
+import imgbg02 from '../img/elem-menu-02.svg';
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props);
@@ -37,7 +38,9 @@ const Navbar = class extends React.Component {
         role="navigation"
         aria-label="main-navigation"
       >
+
         <div className="container">
+
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
               Instituto Técnico Maestro Alfredo Bravo
@@ -57,21 +60,38 @@ const Navbar = class extends React.Component {
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
+            <div className="img-bg-02">
+              <img
+                src={imgbg02}
+                alt="bg02"
+              />
+            </div>
+
             <div className="navbar-end has-text-centered">
-              <Link className="navbar-item" to="/escuela/" className={"navbar-item " } activeClassName={"activa"} partiallyActive={true}>
-              <span>Escuela</span>
-              </Link>
-             
-              <Link className="navbar-item" to="/administracion/" className={"navbar-item "}  activeClassName={"activa"} partiallyActive={true}>
-              <span>Administración</span>
+              <Link className="navbar-item" to="/escuela/" className={"navbar-item "} activeClassName={"activa"} partiallyActive={true}>
+                <span>Escuela</span>
               </Link>
 
-              <Link className="navbar-item" to="/contact/" className={"navbar-item "}  activeClassName={"activa"} partiallyActive={true}>
-              <span>Contacto</span>
+              <Link className="navbar-item" to="/administracion/" className={"navbar-item "} activeClassName={"activa"} partiallyActive={true}>
+                <span>Administración</span>
+              </Link>
+
+              <Link className="navbar-item" to="/contact/" className={"navbar-item "} activeClassName={"activa"} partiallyActive={true}>
+                <span>Contacto</span>
               </Link>
             </div>
 
+
+            <div className="img-bg-01">
+              <img
+                src={imgbg01}
+                alt="bg01"
+              />
+            </div>
+
           </div>
+
+
         </div>
       </nav>
     );
