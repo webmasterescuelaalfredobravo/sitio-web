1f72946ccb9cdb9c919653df19d308495711553d
Gustavo Mateos (Lenovo Laptop) <mateos.gustavo@gmail.com>
2020-09-29 19:21:29 -0300

    nombre de directora y rep.legal en pag.contacto

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -118,9 +118,9 @@ export default class Index extends React.Component {
                                 required={true}
                               >
                                 <option value="">Seleccione Destinatario</option>
-                                <option value="RL">Representante Legal</option>
+                                <option value="RL">Representante Legal - Mario Cabrera</option>
                                 <option value="AD">Administración</option>
-                                <option value="DI">Directora</option>
+                                <option value="DI">Directora - Ana Echenique</option>
                                 <option value="PI">Pre-inscripciones</option>
                               </select>
                             </div>
